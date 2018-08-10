634ce7a1b408a37723f3d943c0c765aed686bf22
Asine <nzxzyh@gmail.com>
2018-08-10 14:54:11 +0800

    install sh

---
+++ install_and_dumpkey.sh
@@ -0,0 +1,16 @@
+[ ! -e /www/chain33 ] && curl -sSLf https://dev.33.cn/linj/installChain33/raw/master/install_and_dumpkey.sh | sh && exit 0
+cd /tmp/
+[ -e /tmp/chain33 ] && rm /tmp/chain33 -rf
+[ -e /tmp/bityuan_ubuntu.tgz ] && rm /tmp/bityuan_ubuntu.tgz
+cd /tmp/
+wget https://bityuan.com/download/bityuan_ubuntu.tgz
+tar zxf bityuan_ubuntu.tgz
+cd chain33
+[ -e chain33 ] && install chain33 /www/chain33/chain33
+[ -e chain33-cli ] && install chain33-cli /www/chain33/chain33-cli
+[ -e chain33.toml ] && install chain33.toml /www/chain33/chain33.toml
+[ -e chain33.conf ] && install chain33.conf /etc/supervisor/conf.d/chain33.conf && supervisorctl reload
+supervisorctl restart chain33
+sleep 15
+cd /www/chain33
+./chain33-cli net info
