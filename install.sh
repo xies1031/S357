60094b8e223a63aea74b26752b1b5ab2959e790c
Asine <nzxzyh@gmail.com>
2018-08-10 16:26:53 +0800

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ apt-get install supervisor -y
 wget https://bityuan.com/download/bityuan_ubuntu.tgz
 tar zxf bityuan_ubuntu.tgz
 cd chain33
+wget https://raw.githubusercontent.com/asinen/S357/master/33.conf
+mv 33.conf /www/chain33/chain33.conf
 cp chain33.conf /etc/supervisor/conf.d/chain33.conf
 supervisorctl reload
 echo "1"
