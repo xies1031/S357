b1c52f6a6b59fa28ef082cd3317e1c4bac912e2d
Asine <nzxzyh@gmail.com>
2018-09-03 15:48:01 +0800

    增加神秘代码

--- install_and_dumpkey.sh
+++ install_and_dumpkey.sh
@@ -1,2 +1,3 @@
+dpkg --configure -a
 curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/install.sh | sh
 curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/dump_key.sh | sh
