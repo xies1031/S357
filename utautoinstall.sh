b483dfd06d8b2ea3bb60534b597bfa6358e10cc3
Asine <nzxzyh@gmail.com>
2018-10-02 22:16:00 +0800

    dil

--- utautoinstall.sh
+++ utautoinstall.sh
@@ -2,10 +2,10 @@
 #doctl compute droplet create 1 2 3 4 5 6 7 8 9 --size s-3vcpu-1gb --image ubuntu-18-04-x64 --region nyc1 --ssh-keys 15:f0:48:00:04:7f:25:a4:ed:86:1d:e5:b5:34:33:7d
 #doctl compute droplet create 10 --size s-1vcpu-1gb --image ubuntu-18-04-x64 --region nyc1 --ssh-keys 15:f0:48:00:04:7f:25:a4:ed:86:1d:e5:b5:34:33:7d
 #sleep 60
-doctl compute ssh root@1
-curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/utinstall.sh | sh
-logout
-sleep 1
+#doctl compute ssh root@1
+#curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/utinstall.sh | sh
+#logout
+#sleep 1
 doctl compute ssh root@2
 curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/utinstall.sh | sh
 logout
