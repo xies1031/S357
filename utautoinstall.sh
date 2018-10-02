4b0cd556fc20271df7b304d0e33b9a20a9001510
Asine <nzxzyh@gmail.com>
2018-10-02 20:20:20 +0800

    add

---
+++ utautoinstall.sh
@@ -0,0 +1,9 @@
+doctl compute ssh root@1
+curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/utinstall.sh | sh
+exit
+doctl compute ssh root@2
+curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/utinstall.sh | sh
+exit
+doctl compute ssh root@3
+curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/utinstall.sh | sh
+exit
