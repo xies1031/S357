ce565d7240a736fd7d714c78e31dfd3d5fd995ff
Asine <nzxzyh@gmail.com>
2018-12-05 12:00:35 +0800

    update

--- 33auto.sh
+++ 33auto.sh
@@ -1,3 +1,3 @@
 doctl compute ssh-key import at --public-key-file ~/.ssh/id_rsa.pub
-doctl compute droplet create 1 2 3 4 5 --size s-1vcpu-2gb --image ubuntu-16-04-x64 --region nyc1 --ssh-keys de:79:e5:33:79:b1:f0:fb:17:84:fb:67:6e:a3:e4:aa
-doctl compute droplet create 6 7 8 9 10 --size s-1vcpu-2gb --image ubuntu-16-04-x64 --region nyc1 --ssh-keys de:79:e5:33:79:b1:f0:fb:17:84:fb:67:6e:a3:e4:aa
+doctl compute droplet create 1 2 3 4 5 --size s-1vcpu-2gb --image ubuntu-18-04-x64 --region nyc1 --ssh-keys de:79:e5:33:79:b1:f0:fb:17:84:fb:67:6e:a3:e4:aa
+doctl compute droplet create 6 7 8 9 10 --size s-1vcpu-2gb --image ubuntu-18-04-x64 --region nyc1 --ssh-keys de:79:e5:33:79:b1:f0:fb:17:84:fb:67:6e:a3:e4:aa
