b50a3848b6aca1035aac8c6f286e24d77b4bed57
Asine <nzxzyh@gmail.com>
2019-02-11 12:31:06 +0800

    update

--- 33install.sh
+++ 33install.sh
@@ -13,10 +13,10 @@ sleep 15
 ./chain33-cli net info
 
 cd ~/chain33
-./chain33-cli seed generate -l 0 | grep seed | cut -d '"' -f 4 > ~/mySeed.txt
+./chain33-cli seed generate -l 0  > ~/mySeed.txt
 cat ~/mySeed.txt | xargs -I{} ./chain33-cli seed save -p 123456 -s "{}"
 ./chain33-cli  wallet unlock  -p 123456 -s wallet -t 0
 sleep 3
 ./chain33-cli account  list  | grep "node award" -B 2 | grep addr   | cut -d '"' -f 4 > ~/myNodeAddr
 cat ~/myNodeAddr | xargs -I{} ./chain33-cli  account dump_key -a {} > ~/myNodePriv
-cat ~/myNodePriv   | grep replystr | cut -d '"' -f 4 | xargs  -I{} echo "Run the cmd in wallet PC: account import_key -l $(date +%m%d/%H%M)____ -k {} "
+cat ~/myNodePriv   | grep replystr | cut -d '"' -f 4 | xargs  -I{} echo "account import_key -l $(date +%m%d/%H%M)____ -k {} "
