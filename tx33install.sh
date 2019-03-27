4bec3ae09d7658609f5b17e2fc485586968d4d0d
Asine <nzxzyh@gmail.com>
2019-03-27 15:09:30 +0800

    up

--- tx33install.sh
+++ tx33install.sh
@@ -1,14 +1,16 @@
+sudo su
+cd /root/
 [ -e chain33 ] && cd chain33 && ./chain33-cli net info && exit 0
-sudo apt-get update
-sudo apt-get install wget -y
-sudo apt-get install supervisor -y
-sudo wget https://bityuan.com/download/bityuan_ubuntu.tgz
-sudo tar zxf bityuan_ubuntu.tgz
-sudo cd chain33
-sudo cp chain33.conf ~/etc/supervisor/conf.d/chain33.conf
-sudo supervisorctl reload
-sudo supervisorctl start chain33
-sudo sleep 15
+apt-get update
+apt-get install wget -y
+apt-get install supervisor -y
+wget https://bityuan.com/download/bityuan_ubuntu.tgz
+tar zxf bityuan_ubuntu.tgz
+cd chain33
+cp chain33.conf /etc/supervisor/conf.d/chain33.conf
+supervisorctl reload
+supervisorctl start chain33
+sleep 15
 ./chain33-cli net info
 
 cd ~/chain33
