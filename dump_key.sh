e83f6aa5f588c5a3068c923c8757ea283ff19eac
xies1031 <44956625+xies1031@users.noreply.github.com>
2019-08-22 11:49:17 +0800

    Update dump_key.sh

--- dump_key.sh
+++ dump_key.sh
@@ -1,9 +1,9 @@
-cd /
-cd /www/chain33
-./chain33-cli seed generate -l 0 | grep seed | cut -d '"' -f 4 > ~/mySeed.txt
-cat ~/mySeed.txt | xargs -I{} ./chain33-cli seed save -p 123456 -s "{}"
-./chain33-cli  wallet unlock  -p 123456 -s wallet -t 0
-sleep 3
-./chain33-cli account  list  | grep "node award" -B 2 | grep addr   | cut -d '"' -f 4 > ~/myNodeAddr
-cat ~/myNodeAddr | xargs -I{} ./chain33-cli  account dump_key -a {} > ~/myNodePriv
-cat ~/myNodePriv   | grep replystr | cut -d '"' -f 4 | xargs  -I{} echo "account import_key -l b -k {} "
+cd ~/chain33
+./chain33-cli seed generate -l 0  > ~/mySeed.txt
+echo "bty123456" > ~/myPass
+cat ~/mySeed.txt | xargs -I{} ./chain33-cli seed save -p bty123456 -s "{}"
+./chain33-cli  wallet unlock  -p bty123456 -s wallet -t 0
+sleep 15
+./chain33-cli account  list  | grep "airdropaddr" -B 2 | grep addr  -w | cut -d '"' -f 4 > ~/myAirdropAddr
+cat ~/myAirdropAddr | xargs -I{} ./chain33-cli  account dump_key -a {} > ~/myAirdropPriv
+cat ~/myAirdropPriv | grep data| cut -d '"' -f 4 | xargs  -I{} echo "account import_key -l ali00001 -k {} "
