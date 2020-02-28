113b7e41a40a909836177d65832d8f8c08a16744
xies1031 <44956625+xies1031@users.noreply.github.com>
2020-02-28 17:52:02 +0800

    Update btylite1.sh

--- btylite1.sh
+++ btylite1.sh
@@ -9,6 +9,7 @@ cd chain33
 cp chain33.conf /etc/supervisor/conf.d/chain33.conf
 rm -rf bityuan.toml
 rm -rf bityuan
+chmod 777 bityuan
 wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/bityuan.toml
 wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/bityuan
 supervisorctl reload
