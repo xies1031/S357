1cca5e0dedada3897a00a66d15e2be78563adf68
xies1031 <44956625+xies1031@users.noreply.github.com>
2020-02-28 18:44:18 +0800

    Create up6.sh

---
+++ up6.sh
@@ -0,0 +1,7 @@
+cd /root/chain33
+rm -rf bityuan.toml
+rm -rf bityuan
+wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/bityuan.toml
+wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/bityuan
+chmod 777 bityuan
+reboot
