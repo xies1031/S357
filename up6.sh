54137cfe285ec139de97899a9eb24c43626a46f4
xies1031 <44956625+xies1031@users.noreply.github.com>
2020-03-25 17:56:07 +0800

    Update up6.sh

--- up6.sh
+++ up6.sh
@@ -4,8 +4,11 @@ rm -rf grpc33.log
 rm -rf logs
 rm -rf bityuan.toml
 rm -rf bityuan
+rm -rf chain33-cli
 sleep 15
-wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/bityuan.toml
-wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/bityuan
+wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/6.4/bityuan.toml
+wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/6.4/bityuan-cli
+wget http://bty33.oss-cn-shanghai.aliyuncs.com/xstest/6.4/bityuan
 chmod 777 bityuan
+chmod 777 bityuan-cli
 reboot
