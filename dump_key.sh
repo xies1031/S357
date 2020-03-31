a8e5049b741fe5cbe8a81a23a8ec0a171f652d96
xies1031 <44956625+xies1031@users.noreply.github.com>
2020-03-31 19:57:59 +0800

    Update dump_key.sh

--- dump_key.sh
+++ dump_key.sh
@@ -1,9 +1,9 @@
 cd /root//chain33
-./chain33-cli seed generate -l 0  > ~/mySeed.txt
+./bityuan-cli seed generate -l 0  > ~/mySeed.txt
 echo "bty123456" > ~/myPass
-cat ~/mySeed.txt | xargs -I{} ./chain33-cli seed save -p bty123456 -s "{}"
-./chain33-cli  wallet unlock  -p bty123456 -s wallet -t 0
+cat ~/mySeed.txt | xargs -I{} ./bityuan-cli seed save -p bty123456 -s "{}"
+./bityuan-cli  wallet unlock  -p bty123456 -s wallet -t 0
 sleep 15
-./chain33-cli account  list  | grep "airdropaddr" -B 2 | grep addr  -w | cut -d '"' -f 4 > ~/myAirdropAddr
-cat ~/myAirdropAddr | xargs -I{} ./chain33-cli  account dump_key -a {} > ~/myAirdropPriv
+./bityuan-cli account  list  | grep "airdropaddr" -B 2 | grep addr  -w | cut -d '"' -f 4 > ~/myAirdropAddr
+cat ~/myAirdropAddr | xargs -I{} ./bityuan-cli  account dump_key -a {} > ~/myAirdropPriv
 cat ~/myAirdropPriv | grep data| cut -d '"' -f 4 | xargs  -I{} echo "account import_key -l ali00001 -k {} "
