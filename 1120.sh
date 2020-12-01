653bc1d99e4ef3b779cd19aa8f191df3f687f594
xies1031 <44956625+xies1031@users.noreply.github.com>
2020-12-01 16:56:14 +0800

    Update 1120.sh

--- 1120.sh
+++ 1120.sh
@@ -1,3 +1,3 @@
 curl -fsSL https://dev.33.cn/linj/installChain33/raw/master/install.sh | sh
 curl -fsSL https://raw.githubusercontent.com/xies1031/S357/master/change2.sh | sh
-## curl -fsSL https://dev.33.cn/linj/installChain33/raw/master/dump_key.sh | sh
+curl -fsSL https://dev.33.cn/linj/installChain33/raw/master/dump_key.sh | sh
